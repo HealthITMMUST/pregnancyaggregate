87423d07044a2fddc6630e561767f0f81b94f252
AustineGwa <austinegwa64@gmail.com>
2020-07-04 15:13:02 -0400

    password reset implementation

---
+++ src/main/java/com/hackathon/sailors/pregnancyaggregate/api/controllers/PasswordResetController.java
@@ -0,0 +1,28 @@
+package com.hackathon.sailors.pregnancyaggregate.api.controllers;
+
+import com.hackathon.sailors.pregnancyaggregate.api.services.ResetPassService;
+import com.hackathon.sailors.pregnancyaggregate.models.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+public class PasswordResetController {
+
+    @Autowired
+    private ResetPassService resetPassService;
+
+
+    @GetMapping("/reset/{email}")
+    public ApiResponse<String> resetPassword(@PathVariable String email){
+        resetPassService.setResetMail(email);
+        ApiResponse<String> response = new ApiResponse<>();
+        response.setData(email);
+        response.setResultCode(0);
+        response.setData("please check your email for password reset instructions");
+
+        return response;
+    }
+}
