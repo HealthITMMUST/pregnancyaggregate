88d93778ed88575aadefc954eeb93c66c75f9e52
AustineGwa <austinegwa64@gmail.com>
2020-07-04 03:46:32 -0400

    initial commit

---
+++ backend/src/main/java/com/hackathon/sailors/pregnancyaggregate/PregnancyaggregateApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.sailors.pregnancyaggregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregnancyaggregateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PregnancyaggregateApplication.class, args);
+	}
+
+}
